e157c9fed9688b9833f1cf0050689cc8563f1700
AFCP <AFCP@MTSVTIL053832>
2025-01-04 22:12:11 -0300

    Style : add responsivity to `CoffeList`

--- src/pages/Home/components/CoffeList/styles.ts
+++ src/pages/Home/components/CoffeList/styles.ts
@@ -9,4 +9,12 @@ export const List = styled.div`
   row-gap: 40px;
   column-gap: 32px;
 
+  @media (max-width: 1000px){
+    grid-template-columns: repeat(3,1fr);
+    row-gap: 28px;
+    column-gap: 11px;
+  }
+  @media (max-width: 550px){
+    grid-template-columns: repeat(2,1fr);
+  }
 `;
